df633b7db986d998301ceb2e6ce315142dd5b694
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-18 00:45:56 -0600

    Rutas Amigables 16

--- routes/users.php
+++ routes/users.php
@@ -11,18 +11,18 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-   echo $_GET['id'];
+   $identificador = $_GET['id'];
    if (isset($_GET['id'])) {
       //Mostrar un post
-      // $sql = $dbConn->prepare("SELECT * FROM usuarios where id_usuario=:id");
-      // $sql->bindValue(':id', $_GET['id']);
-      // $sql->execute();
+      $sql = $dbConn->prepare("SELECT * FROM usuarios where id_usuario=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
 
       
 
-      // header("HTTP/1.1 200 OK");
+      header("HTTP/1.1 200 OK");
       // header('Content-Type: application/json');
-      // echo json_encode($sql->fetch(PDO::FETCH_ASSOC));
+      echo json_encode($sql->fetch(PDO::FETCH_ASSOC));
       exit();
    } else {
       //Mostrar lista de post
