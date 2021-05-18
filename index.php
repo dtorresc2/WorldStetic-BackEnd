ce53dbc6540210d8ed9b4e4c2e5e335da5c28bfa
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-17 21:39:00 -0600

    Iniciando APP 4

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 echo $conn;
+if (mysqli_connect_error()) {
+   die("Conexión a la base de datos fallo " . mysqli_connect_error() . mysqli_connect_errno());
+}
+else {
+   echo 'Funciona';
+}
