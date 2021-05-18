53144074b2fcacff0f5b577a71c0977dd26f219b
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-17 22:28:13 -0600

    Iniciando APP 11

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
 //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("SERVER"));
-// $cleardb_server = $cleardb_url["SERVER"];
-// $cleardb_username = $cleardb_url["USERNAME"];
-// $cleardb_password = $cleardb_url["PASSWORD"];
-// $cleardb_db = substr($cleardb_url["DB"], 1);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["SERVER"];
+$cleardb_username = $cleardb_url["USERNAME"];
+$cleardb_password = $cleardb_url["PASSWORD"];
+$cleardb_db = substr($cleardb_url["DB"], 1);
 
-$cleardb_server = 'us-cdbr-east-03.cleardb.com';
-$cleardb_username = 'b9b0e55ea4f5df';
-$cleardb_password = '90054949';
-$cleardb_db = 'heroku_ae29aed98b86fc3';
+// $cleardb_server = 'us-cdbr-east-03.cleardb.com';
+// $cleardb_username = 'b9b0e55ea4f5df';
+// $cleardb_password = '90054949';
+// $cleardb_db = 'heroku_ae29aed98b86fc3';
 
 $active_group = 'default';
 $query_builder = TRUE;
-
-echo $_ENV["SERVER"];
 
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
