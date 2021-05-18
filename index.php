70747326dc6ac2a6e6c38b1788c883aed80f7d53
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-17 22:16:30 -0600

    Iniciando APP 9

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("SERVER"));
 // $cleardb_server = $cleardb_url["SERVER"];
 // $cleardb_username = $cleardb_url["USERNAME"];
 // $cleardb_password = $cleardb_url["PASSWORD"];
@@ -13,13 +13,13 @@ $cleardb_db = 'heroku_ae29aed98b86fc3';
 
 $active_group = 'default';
 $query_builder = TRUE;
-echo 'Llegue aca '.$cleardb_url;
-echo '\n';
+
+echo $cleardb_url;
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (mysqli_connect_error()) {
-   echo 'Error';
    die("Conexión a la base de datos fallo " . mysqli_connect_error() . mysqli_connect_errno());
 }
 else {
