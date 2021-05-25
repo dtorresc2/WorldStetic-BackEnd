23cd3e21b545f79c81be4be59cf1f74539260766
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-24 23:47:50 -0600

    Rutas - Usuarios 2

---
+++ routes/autenticacion.php
@@ -0,0 +1,50 @@
+<?php
+include "../config/config.php";
+include "../config/utils.php";
+
+include '../lib/password.php';
+
+$dbConn = connect($db);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
+header('Content-Type: application/json');
+
+$mensaje = array(
+   "PASS" => ""
+);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   if (isset($_GET['auth'])) {
+      if ($_GET['auth'] == 'registrar') {
+         $input = file_get_contents('php://input');
+         $input = json_decode($input, true);
+
+         // $hash = password_hash($input['PASS'], PASSWORD_BCRYPT);
+         $hash = password_hash('hola', PASSWORD_BCRYPT);
+         $mensaje['ESTADO'] = $hash;
+
+         header("HTTP/1.1 200 OK");
+
+         echo $hash;
+         exit();
+      }
+
+      if ($_GET['auth'] == 'ingresar') {
+         $input = file_get_contents('php://input');
+         $input = json_decode($input, true);
+
+         header("HTTP/1.1 200 OK");
+
+         if (password_verify('hola', $input['PASS'])) {
+            echo 'Si';
+         } else {
+            echo 'No';
+         }
+         
+         exit();
+      }
+   }
+}
+
+header("HTTP/1.1 400 Bad Request");
--- routes/usuarios.php
+++ routes/usuarios.php
@@ -2,21 +2,27 @@
 include "../config/config.php";
 include "../config/utils.php";
 
+include '../lib/password.php';
+
 $dbConn = connect($db);
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 header('Content-Type: application/json');
 
+$mensaje = array(
+   "PASS" => ""
+);
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
    if (isset($_GET['id'])) {
       //Mostrar un post
       $sql = $dbConn->prepare(
-         "SELECT ". 
-         "id_usuario AS ID_USUARIO, ".
-         "usuario AS USUARIO ".
-         "FROM usuarios ".
-         "where id_usuario=:id"
+         "SELECT " .
+            "id_usuario AS ID_USUARIO, " .
+            "usuario AS USUARIO " .
+            "FROM usuarios " .
+            "where id_usuario=:id"
       );
       $sql->bindValue(':id', $_GET['id']);
       $sql->execute();
@@ -27,10 +33,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
    } else {
       //Mostrar lista de post
       $sql = $dbConn->prepare(
-         "SELECT ". 
-         "id_usuario AS ID_USUARIO, ".
-         "usuario AS USUARIO ".
-         "FROM usuarios "
+         "SELECT " .
+            "id_usuario AS ID_USUARIO, " .
+            "usuario AS USUARIO " .
+            "FROM usuarios "
       );
       $sql->execute();
       $sql->setFetchMode(PDO::FETCH_ASSOC);
@@ -39,6 +45,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       echo json_encode($sql->fetchAll());
       exit();
    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   $input = file_get_contents('php://input');
+   $input = json_decode($input, true);
+
+   $hash = password_hash('hola', PASSWORD_BCRYPT);
+   $mensaje['ESTADO'] = $hash;
+
+   header("HTTP/1.1 200 OK");
+
+   echo $hash;
+   exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+   $input = file_get_contents('php://input');
+   $input = json_decode($input, true);
+
+   if (password_verify('hola', $input['PASS'])) {
+      /* Valid */
+      echo 'Si';
+   } else {
+      /* Invalid */
+      echo 'No';
+   }
+
+   header("HTTP/1.1 200 OK");
+   exit();
+}
+
+if (password_verify($password, $hash)) {
+   /* Valid */
+} else {
+   /* Invalid */
 }
 // https://stackoverflow.com/questions/8291712/using-clean-urls-in-restful-api
 // https://codigonaranja.com/crear-restful-web-service-php
