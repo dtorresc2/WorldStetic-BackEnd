e2266452db2ed2c2af98c8a5669981b7ba486afd
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-24 23:09:44 -0600

    Rutas - Servicios - Actualizando y Eliminando

--- routes/servicios.php
+++ routes/servicios.php
@@ -8,17 +8,16 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 header('Content-Type: application/json');
 
+$mensaje = array(
+   "ESTADO" => "",
+   "MENSAJE" => "",
+   "ID" => 0
+);
+
 // Crear un nuevo post
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    $input = file_get_contents('php://input');
-   // echo $input;
    $input = json_decode($input, true);
-
-   // $sql = "INSERT INTO servicios (descripcion, monto, estado) VALUES(:descripcion, :monto, :estado)";
-   // $statement = $dbConn->prepare($sql);
-   // bindAllValues($statement, $input);
-   // $statement->execute();
-   // $postId = $dbConn->lastInsertId();
 
    $sql = "INSERT INTO servicios (
       descripcion, 
@@ -32,24 +31,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       )";
 
    $stmt = $dbConn->prepare($sql);
-
    $stmt->bindParam(':DESCRIPCION', $input['DESCRIPCION'], PDO::PARAM_STR);
-   $stmt->bindParam(':MONTO', $input['MONTO'], PDO::PARAM_STR);
-   $stmt->bindParam(':ESTADO', $input['ESTADO'], PDO::PARAM_STR);
+   $stmt->bindParam(':MONTO', $input['MONTO'], PDO::PARAM_INT);
+   $stmt->bindParam(':ESTADO', $input['ESTADO'], PDO::PARAM_INT);
    $stmt->execute();
    $postId = $dbConn->lastInsertId();
 
    if ($postId) {
       $respuesta['id'] = $postId;
       header("HTTP/1.1 200 OK");
-      echo json_encode($respuesta);
+
+      $mensaje['ESTADO'] = 1;
+      $mensaje['MENSAJE'] = "Creado Correctamente";
+      $mensaje['ID'] = $respuesta['id'];
+
+      echo json_encode($mensaje);
+      // echo json_encode($respuesta);
       exit();
    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
    if (isset($_GET['id'])) {
-      //Mostrar un post
+
       $sql = $dbConn->prepare(
          "SELECT " .
             " id_servicio AS ID_SERVICIO, " .
@@ -66,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       echo json_encode($sql->fetch(PDO::FETCH_ASSOC));
       exit();
    } else {
-      //Mostrar lista de post
+
       $sql = $dbConn->prepare(
          "SELECT " .
             " id_servicio AS ID_SERVICIO, " .
@@ -83,3 +87,56 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       exit();
    }
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+   $input = $_GET;
+   $id = $input['id'];
+
+   $input = file_get_contents('php://input');
+   $input = json_decode($input, true);
+
+   $sql = "UPDATE servicios SET 
+         descripcion = :DESCRIPCION, 
+         monto = :MONTO, 
+         estado = :ESTADO 
+      WHERE id_servicio = :ID_SERVICIO";
+
+   $stmt = $dbConn->prepare($sql);
+   $stmt->bindParam(':DESCRIPCION', $input['DESCRIPCION'], PDO::PARAM_STR);
+   $stmt->bindParam(':MONTO', $input['MONTO'], PDO::PARAM_INT);
+   $stmt->bindParam(':ESTADO', $input['ESTADO'], PDO::PARAM_INT);
+   $stmt->bindParam(':ID_SERVICIO', $id, PDO::PARAM_INT);
+   $stmt->execute();
+
+   header("HTTP/1.1 200 OK");
+
+   $mensaje['ESTADO'] = 1;
+   $mensaje['MENSAJE'] = "Actualizado Correctamente";
+   $mensaje['ID'] = $id;
+
+   echo json_encode($mensaje);
+   exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+   $input = $_GET;
+   $id = $input['id'];
+
+   $sql = "DELETE FROM servicios
+      WHERE id_servicio = :ID_SERVICIO";
+
+   $stmt = $dbConn->prepare($sql);
+   $stmt->bindParam(':ID_SERVICIO', $id, PDO::PARAM_INT);
+   $stmt->execute();
+
+   header("HTTP/1.1 200 OK");
+
+   $mensaje['ESTADO'] = 1;
+   $mensaje['MENSAJE'] = "Eliminado Correctamente";
+   $mensaje['ID'] = $id;
+
+   echo json_encode($mensaje);
+   exit();
+}
+
+header("HTTP/1.1 400 Bad Request");
