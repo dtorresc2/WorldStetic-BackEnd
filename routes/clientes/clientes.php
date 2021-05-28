d4a7be6bc590ac553dfaf3ff78e9df2b8aef0a02
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-27 23:05:48 -0600

    Headers 5

--- routes/clientes/clientes.php
+++ routes/clientes/clientes.php
@@ -4,7 +4,7 @@ include "../../config/utils.php";
 
 $dbConn = connect($db);
 
-// header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Content-Type: application/json');
