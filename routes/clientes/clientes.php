3153a1e7246466676298737a346d90895c09a1dd
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-27 22:52:27 -0600

    Headers 2

--- routes/clientes/clientes.php
+++ routes/clientes/clientes.php
@@ -9,7 +9,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Credentials', 'true');
-header('Access-Control-Allow-Headers','Content-Type, Authorization');
+header('Access-Control-Allow-Headers','Content-Type, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Methods');
 
 
 $mensaje = array(
