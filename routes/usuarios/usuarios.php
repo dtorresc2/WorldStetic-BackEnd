10e8029c026657b049b39227aa3e9d90900028d3
dtorresc2 <diegotorres_umg@hotmail.com>
2021-05-29 23:44:58 -0600

    Ajustes 3

--- routes/usuarios/usuarios.php
+++ routes/usuarios/usuarios.php
@@ -57,43 +57,119 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   $input = file_get_contents('php://input');
-   $input = json_decode($input, true);
+   if (isset($_GET['id'])) {
+      switch ($_GET['id']) {
+         case 'auth':
+            // Inicio de Sesion
+            $input = file_get_contents('php://input');
+            $input = json_decode($input, true);
 
-   $sql = "INSERT INTO usuarios (
-      usuario, 
-      password, 
-      estado,
-      admin
-   ) 
-   VALUES(
-      :USUARIO, 
-      :PASSWORD, 
-      :ESTADO,
-      1
-      )";
+            $sql = $dbConn->prepare(
+               "SELECT " .
+                  "id_usuario AS ID_USUARIO, " .
+                  "usuario AS USUARIO, " .
+                  "password AS PASSWORD, " .
+                  "estado AS ESTADO, " .
+                  "admin AS ADMIN " .
+                  "FROM usuarios " .
+                  "WHERE usuario=:USUARIO"
+            );
+            $sql->bindValue(':USUARIO', $input['USUARIO']);
+            $sql->execute();
 
-   $hash = password_hash($input['PASSWORD'], PASSWORD_BCRYPT);
+            header("HTTP/1.1 200 OK");
+            $filas = $sql->fetch(PDO::FETCH_ASSOC);
 
-   $stmt = $dbConn->prepare($sql);
-   $stmt->bindParam(':USUARIO', $input['USUARIO'], PDO::PARAM_STR);
-   $stmt->bindParam(':PASSWORD', $hash, PDO::PARAM_STR);
-   $stmt->bindParam(':ESTADO', $input['ESTADO'], PDO::PARAM_INT);
-   $stmt->execute();
-   $postId = $dbConn->lastInsertId();
+            if ($sql->rowCount() > 0) {
+               if (password_verify($input['PASSWORD'], $filas['PASSWORD'])) {
+                  $mensaje['ESTADO'] = 1;
+                  $mensaje['MENSAJE'] = "Usuario Autenticado";
+                  $mensaje['ID'] = (int)$filas['ID_USUARIO'];
+               } else {
+                  $mensaje['ESTADO'] = 0;
+                  $mensaje['MENSAJE'] = "Credenciales Incorrectas";
+                  $mensaje['ID'] = -1;
+               }
+            } else {
+               $mensaje['ESTADO'] = 0;
+               $mensaje['MENSAJE'] = "Usuario No Existente";
+               $mensaje['ID'] = -1;
+            }
 
-   if ($postId) {
-      $respuesta['id'] = $postId;
+            echo json_encode($mensaje);
+            exit();
+            break;
+         default:
+            header("HTTP/1.1 400 Bad Request");
+            exit();
+            break;
+      }
+   } else {
+      $input = file_get_contents('php://input');
+      $input = json_decode($input, true);
+
+      $sql = "INSERT INTO usuarios (
+         usuario, 
+         password, 
+         estado,
+         admin
+      ) 
+      VALUES(
+         :USUARIO, 
+         :PASSWORD, 
+         :ESTADO,
+         1
+         )";
+
+      $hash = password_hash($input['PASSWORD'], PASSWORD_BCRYPT);
+
+      $stmt = $dbConn->prepare($sql);
+      $stmt->bindParam(':USUARIO', $input['USUARIO'], PDO::PARAM_STR);
+      $stmt->bindParam(':PASSWORD', $hash, PDO::PARAM_STR);
+      $stmt->bindParam(':ESTADO', $input['ESTADO'], PDO::PARAM_INT);
+      $stmt->execute();
+      $postId = $dbConn->lastInsertId();
+
+      if ($postId) {
+         $respuesta['id'] = $postId;
+         header("HTTP/1.1 200 OK");
+
+         $mensaje['ESTADO'] = 1;
+         $mensaje['MENSAJE'] = "Creado Correctamente";
+         $mensaje['ID'] = $respuesta['id'];
+
+         echo json_encode($mensaje);
+         exit();
+      }
+      exit();
+   }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+   if (isset($_GET['id'])) {
+      $input = file_get_contents('php://input');
+      $input = json_decode($input, true);
+
+      $sql = "UPDATE usuarios SET 
+         usuario = :USUARIO
+         WHERE id_usuario = :ID_USUARIO";
+
+      $stmt = $dbConn->prepare($sql);
+      $stmt->bindParam(':USUARIO', $input['USUARIO'], PDO::PARAM_STR);
+      $stmt->bindParam(':ID_USUARIO', $_GET['id'], PDO::PARAM_INT);
+      $stmt->execute();
+
+      $ID = $_GET['id'];
+
       header("HTTP/1.1 200 OK");
 
       $mensaje['ESTADO'] = 1;
-      $mensaje['MENSAJE'] = "Creado Correctamente";
-      $mensaje['ID'] = $respuesta['id'];
+      $mensaje['MENSAJE'] = "Actualizado Correctamente";
+      $mensaje['ID'] = $ID;
 
       echo json_encode($mensaje);
       exit();
    }
-   exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
